f14531f0d2021451cedb0ef28bc2daebf744f351
septiarejeki <septiasri22@gmail.com>
2024-02-27 15:52:08 +0700

    ujikom

---
+++ src/views/Home.vue
@@ -0,0 +1,213 @@
+<template>
+<br><br><br>
+
+<div>
+        <img style="margin: 20px;" class="h-auto max-w-full rounded-lg" src="../assets/img/bg.webp" height="95%" width="95%" alt="">
+    </div>
+    
+    <p align="center" class="text-2xl font-semibold text-gray-900 dark:text-white"><span class="italic">Menu</span></p>
+      <br>
+    
+      <section class="py-20">
+  <div class="mx-auto grid max-w-screen-xl grid-cols-1 gap-6 p-6 md:grid-cols-2 lg:grid-cols-5">
+    <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+      <a href="#">
+        <div class="relative flex items-end overflow-hidden rounded-xl">
+          <img src="../assets/img/tiramisu.jpg" alt="Hotel Photo" />
+          <div class="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+            <span class="text-slate-400 ml-1 text-sm">4.9</span>
+          </div>
+        </div>
+        <div class="mt-1 p-2">
+          <h2 class="text-slate-700">The Hilton Hotel</h2>
+          <p class="text-slate-400 mt-1 text-sm">Lisbon, Portugal</p>
+        </div>
+      </a>
+    </article>
+
+    <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+      <a href="#">
+        <div class="relative flex items-end overflow-hidden rounded-xl">
+          <img src="../assets/img/cuitbogel.PNG" alt="Hotel Photo" />
+          <div class="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+            <span class="text-slate-400 ml-1 text-sm">4.9</span>
+          </div>
+        </div>
+
+        <div class="mt-1 p-2">
+          <h2 class="text-slate-700">The Hilton Hotel</h2>
+          <p class="text-slate-400 mt-1 text-sm">Lisbon, Portugal</p>
+
+          <div class="mt-3 flex items-end justify-between">
+          </div>
+        </div>
+      </a>
+    </article>
+
+    <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+      <a href="#">
+        <div class="relative flex items-end overflow-hidden rounded-xl">
+          <img src="../assets/img/puding.PNG" alt="Hotel Photo" />
+          <div class="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+            <span class="text-slate-400 ml-1 text-sm">4.9</span>
+          </div>
+        </div>
+
+        <div class="mt-1 p-2">
+          <h2 class="text-slate-700">The Hilton Hotel</h2>
+          <p class="text-slate-400 mt-1 text-sm">Lisbon, Portugal</p>
+
+          <div class="mt-3 flex items-end justify-between">
+          </div>
+        </div>
+      </a>
+    </article>
+
+    <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+      <a href="#">
+        <div class="relative flex items-end overflow-hidden rounded-xl">
+          <img src="../assets/img/cu.PNG" alt="Hotel Photo" />
+          <div class="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+            <span class="text-slate-400 ml-1 text-sm">4.9</span>
+          </div>
+        </div>
+
+        <div class="mt-1 p-2">
+          <h2 class="text-slate-700">The Hilton Hotel</h2>
+          <p class="text-slate-400 mt-1 text-sm">Lisbon, Portugal</p>
+
+          <div class="mt-3 flex items-end justify-between">
+          </div>
+        </div>
+      </a>
+    </article>
+
+    <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+      <a href="#">
+        <div class="relative flex items-end overflow-hidden rounded-xl">
+          <img src="../assets/img/cuitbogel.PNG" alt="Hotel Photo" />
+          <div class="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+            <span class="text-slate-400 ml-1 text-sm">4.9</span>
+          </div>
+        </div>
+
+        <div class="mt-1 p-2">
+          <h2 class="text-slate-700">The Hilton Hotel</h2>
+          <p class="text-slate-400 mt-1 text-sm">Lisbon, Portugal</p>
+
+          <div class="mt-3 flex items-end justify-between">
+          </div>
+        </div>
+      </a>
+    </article>
+
+
+  </div>
+</section>
+
+
+    <div class="grid grid-cols-5 gap-4">
+        <div>
+          <a href="/src/views/Cheseecuit_kategori.vue">
+            <img class="h-auto max-w-full rounded-lg" src="../assets/img/tiramisu.jpg" alt="">
+          </a>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="../assets/img/cuitbogel.PNG" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="../assets/img/puding.PNG" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="../assets/img/cpie.PNG" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="../assets/img/chewyy.PNG" alt="">
+        </div>
+    </div>
+
+<br>
+<div class="mx-auto my-10 w-screen max-w-screen-xl px-4">
+  <div class="flex rounded-xl bg-white py-2 px-2 text-gray-700 shadow-lg shadow-pink-200">
+    <div class="my-auto mr-4 p-3 text-blue-500">
+      <h1 class="text-2xl font-bold">Apa itu Bandung Chesecuit?</h1>
+      <p class="mt-4 text-lg"> Bandung chesecuit merupakan pelopor pertama dalam menciptakan
+        pengalaman nikmat dari biskuit istimewa yang dipadukan dengan krim keju lezat,
+         meleleh di mulut, dan diperkaya dengan dengan beragam topping untuk menciptakan 
+         sensasi cita rasa yang unik. pengalaman nikmat ini tidak hanya menjadi daya tarik di kota Bandung,
+        tetapi juga telah menciptakan keinginan yang mendalam pencinta keju di seluruh indonesia</p>
+      <button type="submit" class="group mt-4 mr-2 mb-4 flex cursor-pointer text-left text-xl font-semibold leading-tight text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-7 w-7 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+        </svg>
+      </button>
+    </div>
+    <div class="hidden h-72 w-1/2 flex-shrink-0 justify-center lg:flex">
+      <img class="" src="../assets/img/about.jpg" alt="" />
+    </div>
+  </div>
+</div>
+
+
+
+<br><br>
+ <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/buah.jfif" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/strawberry.webp" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets//img/greentea.webp" alt="">
+    </div>
+</div>
+
+<br><br>
+ <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/choco.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/pisang.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets//img/tiramisu.jpg" alt="">
+    </div>
+</div>
+
+<br>
+
+<div class="grid grid-cols-1 md:grid-cols-3 gap-4 m-20">
+<video class="w-96" controls autoplay muted>
+  <source src="../assets/vidio/cuit.mp4"  type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+<video class="w-96" controls autoplay muted>
+  <source src="../assets/vidio/cuit2.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+<video class="w-96" controls autoplay muted>
+  <source src="../assets/vidio//cuit3.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+</div> 
+
+
+</template>
+
